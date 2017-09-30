480e4014183bfbd83fcaba78520b324b4fe750d9
comunit <comunit76@gmail.com>
2017-09-30 03:24:41 +0100

    files added

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+// Get Homepage
+router.get('/', ensureAuthenticated, function(req, res){
+	res.render('index');
+});
+
+// Register
+router.get('/register', function(req, res){
+	res.render('register');
+});
+
+// Login
+router.get('/login', function(req, res){
+	res.render('login');
+});
+
+router.get('/logout', function (req, res) {
+	req.logout();
+	req.flash('success_msg', 'You are logged out');
+
+	res.redirect('/login');
+});
+
+function ensureAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	} else {
+		res.redirect('/login');
+	}
+}
+
+module.exports = router;
